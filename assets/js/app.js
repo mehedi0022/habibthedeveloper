08e6a9384fcc3b24db8497a0d78a9485f10625ee
mehedi0022 <mdmehedihassan0022@gmail.com>
2022-07-01 08:24:46 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+/*----------Skills Tab----------*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]');
+    
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+            
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove("skills_active")
+            })
+            target.classList.add('skills_active')
+
+
+            tabs.forEach(tab => {
+                tab.classList.remove("skills_active")
+            })
+            tab.classList.add('skills_active')
+        })
+    })
+ 
+
+
+document.addEventListener("click", (e) =>  {
+    if(e.target.classList.contains("work_button")) {
+        togglePortfolioPopup();
+        portfolioItemDetaisl(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+document.querySelector(".portfolio_popup-close").addEventListener('click', togglePortfolioPopup);
+
+function portfolioItemDetaisl(portfolioItem){
+    document.querySelector(".pp_thumbline img").src = portfolioItem.querySelector(".work_img").src;
+    document.querySelector(".fortfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
+    document.querySelector(".portfolio_popup-body").innerHTML = portfolioItem.querySelector(".portfolio_item-details").innerHTML;
+}
+
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalClose = document.querySelectorAll('.services_modal-close');
+
+
+let modal = function(modalclick) {
+    modalViews[modalclick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+})
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
+
