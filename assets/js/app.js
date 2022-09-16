bd762ae60ec4438a42291521a90dcaed97dbe47b
mehedi0022 <mdmehedihassan0022@gmail.com>
2022-09-16 09:01:47 +0600

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,3 +59,21 @@ modalClose.forEach((modalClose) => {
     })
 })
 
+/*------Input-----*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
